a44a1822e25df0d059096213f574a7752ed75f40
Giovanni Rodriguez G <34606945+grg8881m@users.noreply.github.com>
2020-10-28 16:48:46 -0500

    Add files via upload

---
+++ string.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+
+{
+    char string1[60];
+
+    printf("Escribe una frase\n");
+    gets(string1);
+
+    strrev(string1);
+
+    printf("el string al reves es: %s\n", string1);
+
+    return 0;
+
+}
